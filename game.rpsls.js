37d9a78080711b2a4b4709cbc97a134bfa98aff4
unknown <larserlandson5@gmail.com>
2020-06-23 22:27:25 -0500

    Added logic to run more games

--- game.rpsls.js
+++ game.rpsls.js
@@ -1,34 +1,40 @@
 "use strict";
 
-const rock=1;         // crushes Scissors  
-const paper=2;         // covers Rock 
-const scissors=3;      // cuts Paper 
-const lizard=null;        // poisons Spock 
-const spock=null;         //smashes Scissors 
+const rock = 1;         // crushes Scissors  
+const paper = 2;         // covers Rock 
+const scissors = 3;      // cuts Paper 
+const lizard = null;        // poisons Spock 
+const spock = null;         //smashes Scissors 
 
 class Game {
     constructor() {
         this.playerOne = new Player("Lars");
         this.playerTwo = new Player("Damon");
+        this.numberOfGames = prompt("How many games do you want to play?")
+        //number of games (best of style)
+        //array of players
+        //score
     }
     rungame() {
-        this.playerOne.gesture = this.playerOne.getGesture();
-        this.playerTwo.gesture = this.playerTwo.getGesture();
-        this.pickWinner();
-        console.log(this.playerOne.score,this.playerTwo.score);
-        //call the decide who wins method
+        for (let i = 0; this.numberOfGames > i; i++) {
+            this.playerOne.gesture = this.playerOne.getGesture();
+            this.playerTwo.gesture = this.playerTwo.getGesture();
+            this.pickWinner();
+            console.log(this.playerOne.score, this.playerTwo.score);
+            //call the decide who wins method
 
-
+        }
     }
-    pickWinner(){
-        if(this.playerOne.gesture < this.playerTwo.gesture){
+    pickWinner() {
+        //logic needed for tie
+        if (this.playerOne.gesture < this.playerTwo.gesture) {
             this.playerTwo.score++;
         }
-        else{
+        else {
             this.playerOne.score++;
         }
     }
-    
+
     //add to a players score
     //decide who wins for total score
     //later this would be our rng computer for one player
@@ -43,10 +49,9 @@ class Player {
         this.gesture = null;
     }
     getGesture() {
-        console.log("whoak")
         let gesture = prompt(`${this.name}, Pick only one. rock, paper, scissors, lizard, spock`);
         return gesture.toLowerCase();
-        
+
 
     }
 }
